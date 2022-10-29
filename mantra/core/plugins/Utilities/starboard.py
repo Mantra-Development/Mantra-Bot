122ca13ea3786125e293c778232f3ab383419d4b
Nishant Sapkota <snishant306@gmail.com>
2022-10-29 13:31:17 +0545

    chore(lint): fix lint errors

--- mantra/core/plugins/Utilities/starboard.py
+++ mantra/core/plugins/Utilities/starboard.py
@@ -4,10 +4,12 @@ import hikari
 import lightbulb
 
 from mantra.core.models import StarboardEntries, StarConfig
+from mantra.core.utils import Colors, CommandError, Emojis
+
 from . import starboard_embed_handler
-from mantra.core.utils import Colors,Emojis,CommandError
 
 starboard = lightbulb.Plugin("Starboard", "Plugin containing starboard commands")
+
 
 @starboard.command
 @lightbulb.command("starboard", "Starboard command group")
@@ -17,18 +19,23 @@ async def starboard_command(_: lightbulb.Context) -> None:
 
 
 @starboard_command.child
-@lightbulb.option("channel", "The channel to set as starboard channel", type=hikari.GuildChannel, channel_types=hikari.ChannelType.GUILD_TEXT)
+@lightbulb.option(
+    "channel",
+    "The channel to set as starboard channel",
+    type=hikari.GuildChannel,
+    channel_types=hikari.ChannelType.GUILD_TEXT,
+)
 @lightbulb.option("threshold", "The star threshold for the server", type=int)
 @lightbulb.command("configure", "Configure starboard for the server", pass_options=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def configure_command(ctx: lightbulb.Context, channel: hikari.GuildTextChannel, threshold: int) -> None:
+async def configure_command(
+    ctx: lightbulb.Context, channel: hikari.GuildTextChannel, threshold: int
+) -> None:
     model = await StarConfig.get_or_none(guild_id=ctx.guild_id)
     if model is None:
         await StarConfig.create(
-                guild_id=ctx.guild_id,
-                channel_id=channel.id,
-                star_count=threshold
-                )
+            guild_id=ctx.guild_id, channel_id=channel.id, star_count=threshold
+        )
     else:
         model.channel_id = channel.id
         model.star_count = threshold
@@ -40,14 +47,13 @@ async def configure_command(ctx: lightbulb.Context, channel: hikari.GuildTextCha
             description=f"{Emojis.SUCCESS} Starboard configured successfully!\n\n**Channel**: <#{channel.id}>\n**Threshold:** {threshold}",
             timestamp=datetime.now().astimezone(),
         ),
-        flags=hikari.MessageFlag.EPHEMERAL
+        flags=hikari.MessageFlag.EPHEMERAL,
     )
-
 
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(starboard)
 
+
 def unload(bot: lightbulb.BotApp) -> None:
     bot.remove_plugin(starboard)
-
