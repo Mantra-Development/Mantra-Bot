c9a6238deb38d579b6a8fb51a89e71666c10d41b
Nishant Sapkota <snishant306@gmail.com>
2022-11-02 16:49:39 +0545

    feat: add some more fun commands

--- mantra/core/plugins/Fun/fun.py
+++ mantra/core/plugins/Fun/fun.py
@@ -1,3 +1,8 @@
+import asyncio
+import random
+from datetime import datetime
+
+import aiohttp
 import hikari
 import lightbulb
 
@@ -29,6 +34,76 @@ async def urban_command(ctx: lightbulb.Context, term: str) -> None:
     navigator = CustomPaginator(ctx, pages=fields)
     await navigator.send(ctx.interaction, ephemeral=True)
     await navigator.wait()
+
+
+@fun.command
+@lightbulb.option("word", "The word to reverse")
+@lightbulb.command("reverse", "Reverse a given word", pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def reverse_command(ctx: lightbulb.Context, word: str) -> None:
+    reversed = word[::-1]
+
+    await ctx.respond(
+        embed=hikari.Embed(
+            description=f"**Original Word**: `{word}`\n**Reversed Word**: `{reversed}`",
+            color=Colors.SUCCESS,
+        ),
+        flags=hikari.MessageFlag.EPHEMERAL,
+    )
+
+
+@fun.command
+@lightbulb.command("flip", "Flip a coin")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def flip_coin_command(ctx: lightbulb.Context) -> None:
+    choices = {
+        "Heads": "https://cdn.discordapp.com/attachments/1026861948274483221/1037314925074325594/heads.png",
+        "Tails": "https://cdn.discordapp.com/attachments/1026861948274483221/1037314924780728370/tails.png",
+    }
+
+    choice = random.choice(list(choices.keys()))
+    await ctx.respond(
+        embed=hikari.Embed(title="Flipping a coin...", color=Colors.INFO).set_image(
+            "https://i.pinimg.com/originals/d7/49/06/d74906d39a1964e7d07555e7601b06ad.gif"
+        )
+    )
+
+    await asyncio.sleep(2)
+
+    await ctx.edit_last_response(
+        embed=hikari.Embed(
+            title=f"You got {choice}!",
+            color=Colors.SUCCESS,
+        )
+        .set_image(choices[choice])
+        .set_footer(
+            text="Flipped a coin in Nepali Style ;)",
+            icon=ctx.author.avatar_url,
+        )
+    )
+
+
+@fun.command
+@lightbulb.option("question", "The question to ask the magic 8Ball")
+@lightbulb.command("8ball", "Ask the magic 8Ball some questions", pass_options=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ball_command(ctx: lightbulb.Context, question: str) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://www.eightballapi.com/api") as response:
+            data = await response.json()
+
+    await ctx.respond(
+        embed=hikari.Embed(
+            title="The Magic 8Ball responded...",
+            description=f"**❓ Question:**\n```{question}```\n**💬 Answer:**\n```{data['reading']}```",
+            color=Colors.SUCCESS,
+            timestamp=datetime.now().astimezone(),
+        ).set_footer(
+            f"Requested by {ctx.author}",
+            icon=ctx.author.avatar_url,
+        ),
+        flags=hikari.MessageFlag.EPHEMERAL,
+    )
 
 
 def load(bot: lightbulb.BotApp) -> None:
